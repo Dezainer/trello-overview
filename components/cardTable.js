99f908f42e67228de64b89101ed63bd45773abcf
Dezainer <gameboymasterz@gmail.com>
2021-01-20 17:21:31 -0300

    Done

--- components/cardTable.js
+++ components/cardTable.js
@@ -1,57 +1,48 @@
+import TableContainer from '@material-ui/core/TableContainer'
 import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
 import Chip from '@material-ui/core/Chip'
-import LinearProgress from '@material-ui/core/LinearProgress'
+import moment from 'moment'
 
-const CardTable = ({ cards }) => (
-  <TableContainer component={Paper}>
+const CardTable = ({ date, cards }) => (
+  <TableContainer component={Paper} className='mt-8'>
     <Table>
       <TableHead>
         <TableRow>
-          <TableCell style={{ width: '20%' }}>Board</TableCell>
-          <TableCell>Card</TableCell>
+          <TableCell colSpan={2}>
+            {date !== 'null'
+              ? moment(date).calendar().split(' ')[0]
+              : 'Sem Data'}
+          </TableCell>
         </TableRow>
       </TableHead>
-      {cards
-        ? (
-          <TableBody>
-            {cards.map(card => (
-              <TableRow key={card.id}>
-                <TableCell>
-                  <a href={card.board.url} target='_blank'>
-                    <Chip
-                      style={{
-                        cursor: 'pointer',
-                        backgroundColor: card.board.prefs.backgroundTopColor,
-                        color: 'white'
-                      }}
-                      label={card.board.name}
-                    />
-                  </a>
-                </TableCell>
-                <TableCell>
-                  <a href={card.url} target='_blank'>
-                    {card.name}
-                  </a>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        )
-        : (
-          <TableBody>
-            <TableRow>
-              <TableCell colSpan={2}>
-                <LinearProgress />
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        )}
+      <TableBody>
+        {cards.map(card => (
+          <TableRow key={card.id}>
+            <TableCell style={{ width: '20%' }}>
+              <a href={card.board.url} target='_blank'>
+                <Chip
+                  style={{
+                    cursor: 'pointer',
+                    backgroundColor: card.board.prefs.backgroundTopColor,
+                    color: 'white'
+                  }}
+                  label={card.board.name}
+                />
+              </a>
+            </TableCell>
+            <TableCell>
+              <a href={card.url} target='_blank'>
+                {card.name}
+              </a>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
   </TableContainer>
 )
